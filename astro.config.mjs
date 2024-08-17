f71b0d912a35cc80794c2aca92e1b23eb819ebc8
ssstakahashi <50137713+ssstakahashi@users.noreply.github.com>
2024-08-18 08:35:27 +0900

    0.0.4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
+import mdx from '@astrojs/mdx';
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [tailwind(), mdx()],
+    content: {
+        collections: {
+            posts: './src/pages/blog/posts',
+        },
+    },
     output: "server",
     adapter: cloudflare()
 });
